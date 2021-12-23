3ea1d07eeec32837b9aee76124832c39b7605574
unknown <hassanajmal531@gmail.com>
2021-12-23 12:03:15 +0500

    trained nb model

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,17 +1,22 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.preprocessing import normalize
 import dataProcessing
 
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 x , y = dataProcessing.DataProcessing()
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.50, random_state = 42)
 
-# X_train = sc.fit_transform(X_train)
-# X_test = sc.transform(X_test)
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
+print(X_train.shape)
+classifier = MultinomialNB()
+classifier.fit(normalize(X_train), y_train)
+
 y_pred = classifier.predict(X_test)
+#nx, ny = dataProcessing.testing(" thank you for shopping with us gifts for all occasions free gift with NUMBER NUMBER purchase for a limited time only receive this NUMBER plush santa bear free with your purchase of NUMBER NUMBER or more when your order totals NUMBER NUMBER or more order must be NUMBER NUMBER or more before shipping and handling this santa bear is added to your cart for free while supplies last mary s store would like to thank you for being a valued customer as our way of saying thanks to you the customer we are offering a NUMBER discount on all purchases made during the month of november just enter the word thanks in the discount code box during checkout to receive your automatic NUMBER discount hyperlink click here hyperlink to enter hyperlink mary s store if you do not wish to receive further discounts please hyperlink click here and type remove in the subject line ");
+
+
+
 print("acuracy of the model is")
 print(accuracy_score(y_test, y_pred))
--- dataProcessing.py
+++ dataProcessing.py
@@ -1,32 +1,48 @@
 import pandas as p
 from sklearn import preprocessing
 from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+from sklearn.preprocessing import normalize
 
 from sklearn import feature_extraction
 
 
 def DataProcessing():
     data = p.read_csv("spam.csv", encoding='latin-1')
-    data.drop_duplicates(inplace= True)
+    #data.drop_duplicates(inplace= True)
     data["email"].fillna("hello", inplace=True) 
     data["class"].fillna("ham" , inplace=True)
 
     
-    x = data.iloc[:, 1].values
-    y = data.iloc[:, 0].values
-    index = 0;
-    for i in y:
-        y[index] = 1 if i == "spam" else 0
-        index += 1
+    x = data.iloc[:, 1]
+    y = data["class"]
 
+    y = y.map({"spam":1, "ham":0})
+
+   # print(y)
+    # index = 0;
+    # for i in y:
+    #     y[index] = 1 if i == "spam" else 0
+    #     index += 1
     features = feature_extraction.text.CountVectorizer(stop_words="english")
     x = features.fit_transform(x);
+    x= normalize(x)
 
+   # print(np.shape(x))
+   # print(np.shape(y))
     return x,y
 
-   
+def testing(email):
+    email = [email]
+    feature = feature_extraction.text.CountVectorizer(stop_words="english")
+    email = feature.fit_transform(email)
+    print(email.shape)
+    y = [1]
+    return email, y
 
-x, y = DataProcessing()
+
+
+testing(" thank you for shopping with us gifts for all occasions free gift with NUMBER NUMBER purchase for a limited time only receive this NUMBER plush santa bear free with your purchase of NUMBER NUMBER or more when your order totals NUMBER NUMBER or more order must be NUMBER NUMBER or more before shipping and handling this santa bear is added to your cart for free while supplies last mary s store would like to thank you for being a valued customer as our way of saying thanks to you the customer we are offering a NUMBER discount on all purchases made during the month of november just enter the word thanks in the discount code box during checkout to receive your automatic NUMBER discount hyperlink click here hyperlink to enter hyperlink mary s store if you do not wish to receive further discounts please hyperlink click here and type remove in the subject line ")
 
 
 
