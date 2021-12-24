4037daba28042145eb053298bfcdb88cc37ee24f
unknown <hassanajmal531@gmail.com>
2021-12-24 22:55:44 +0500

    added naivebayes implementation and cleaned data

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,12 +1,13 @@
+from sklearn import feature_extraction
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.preprocessing import normalize
 import dataProcessing
 
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score
 
-x , y = dataProcessing.DataProcessing()
+x , y , BagOfWords= dataProcessing.DataProcessing()
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.50, random_state = 42)
 
 print(X_train.shape)
@@ -14,9 +15,21 @@ classifier = MultinomialNB()
 classifier.fit(normalize(X_train), y_train)
 
 y_pred = classifier.predict(X_test)
-#nx, ny = dataProcessing.testing(" thank you for shopping with us gifts for all occasions free gift with NUMBER NUMBER purchase for a limited time only receive this NUMBER plush santa bear free with your purchase of NUMBER NUMBER or more when your order totals NUMBER NUMBER or more order must be NUMBER NUMBER or more before shipping and handling this santa bear is added to your cart for free while supplies last mary s store would like to thank you for being a valued customer as our way of saying thanks to you the customer we are offering a NUMBER discount on all purchases made during the month of november just enter the word thanks in the discount code box during checkout to receive your automatic NUMBER discount hyperlink click here hyperlink to enter hyperlink mary s store if you do not wish to receive further discounts please hyperlink click here and type remove in the subject line ");
-
-
-
 print("acuracy of the model is")
 print(accuracy_score(y_test, y_pred))
+
+email = "it has come to our attention that your citizensr bank account information needs to be updated as part of our continuing commitment to protect your account and to reduce the instance of fraud on our website . if you could please take 5 - 10 minutes out of your online experience and renew your records you will not run into any future problems with the online service . however , failure to confirm your records may result in your account suspension .you have confirmed your account records your internet banking service will not be interrupted and will continue as normal .to confirm your bank account records please click here .note :this e - mail was sent on behalf of the online banking community , if you do not have an online banking account with charterr one then this message does not apply to you and you may ignore this message .thank you for your time ,citizensr financial group ."
+email1 = email
+email = [email]
+features = feature_extraction.text.CountVectorizer(vocabulary=BagOfWords)
+email = features.fit_transform(email)
+Y = classifier.predict(email)
+
+if(Y[0] == 1):
+    print("the given email is spam")
+else:
+    print("the given email is not spam")
+
+
+
+
--- dataProcessing.py
+++ dataProcessing.py
@@ -1,73 +1,39 @@
+import nltk
 import pandas as p
-from sklearn import preprocessing
-from sklearn.preprocessing import OneHotEncoder
-import numpy as np
-from sklearn.preprocessing import normalize
 from sklearn import feature_extraction
-import pandas as pd
-import numpy as np
-from sklearn.feature_selection import SelectKBest, VarianceThreshold
-from sklearn.feature_selection import chi2
+from csv import DictWriter
 
-def abc():
-    data = pd.read_csv("spam.csv", encoding = "latin-1")
-    X = data.iloc[:, 1]  
-    y = data.iloc[:,0]  
-    y = y.map({"spam":1, "ham":0})  
-    features = feature_extraction.text.CountVectorizer(stop_words="english")
-    x = features.fit_transform(X);
-    bestfeatures = SelectKBest(score_func=chi2, k=10)
-    fit = bestfeatures.fit(x,y)
-    dfscores = pd.DataFrame(fit.scores_)
-    dfcolumns = pd.DataFrame(X)
-    featureScores = pd.concat([dfcolumns,dfscores],axis=1)
-    featureScores.columns = ['Specs','Score']  
-    print(featureScores.nlargest(10,'Score'))  
 
+
+
+from nltk.corpus import stopwords
+
+BagOfWords = list()
 
 def DataProcessing():
+
     data = p.read_csv("spam.csv", encoding='latin-1')
     #data.drop_duplicates(inplace= True)
     data["email"].fillna("hello", inplace=True) 
     data["class"].fillna("ham" , inplace=True)
-
-    
     x = data.iloc[:, 1]
+    x = x.str.replace("[^a-zA-Z]"," ")
     y = data["class"]
-
     y = y.map({"spam":1, "ham":0})
-
-   # print(y)
-    # index = 0;
-    # for i in y:
-    #     y[index] = 1 if i == "spam" else 0
-    #     index += 1
-    features = feature_extraction.text.CountVectorizer(stop_words="english")
-    x = features.fit_transform(x);
-    print(x.shape)
-
-    s = VarianceThreshold(0.05)
-    x = s.fit_transform(x)
-    print(s.get_feature_names_out())
-
+    features = feature_extraction.text.CountVectorizer(analyzer= "word", stop_words="english", max_features= 3000)
+    x= features.fit_transform(x);
     
+    BagOfWords = features.get_feature_names_out()
+ 
+    return x,y,BagOfWords
 
-   # print(np.shape(x))
-   # print(np.shape(y))
-    return x,y
-
-def testing(email):
-    email = [email]
-    feature = feature_extraction.text.CountVectorizer(stop_words="english")
-    email = feature.fit_transform(email)
-    print(email.shape)
-    print(feature.get_feature_names_out())
-    y = [1]
-    return email, y
-
-
-
-testing(" thank you for shopping with us gifts for all occasions free gift with NUMBER NUMBER purchase for a limited time only receive this NUMBER plush santa bear free with your purchase of NUMBER NUMBER or more when your order totals NUMBER NUMBER or more order must be NUMBER NUMBER or more before shipping and handling this santa bear is added to your cart for free while supplies last mary s store would like to thank you for being a valued customer as our way of saying thanks to you the customer we are offering a NUMBER discount on all purchases made during the month of november just enter the word thanks in the discount code box during checkout to receive your automatic NUMBER discount hyperlink click here hyperlink to enter hyperlink mary s store if you do not wish to receive further discounts please hyperlink click here and type remove in the subject line ")
+def  writeNewEmailToCSV(label, email):
+    headers = {"class","email"}
+    obj = {"class":label,"email":email}
+    with open("spam.csv","a",newline="") as fileObject:
+        dictWriter = DictWriter(fileObject, fieldnames = headers)
+        dictWriter.writerow(obj)
+        fileObject.close()
 
 
 
